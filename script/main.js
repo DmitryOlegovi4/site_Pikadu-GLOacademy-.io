de0a6e2514b07e3f6c5ca34f4491cece0a43b2e6
DmitryOlegovi4 <62551402+DmitryOlegovi4@users.noreply.github.com>
2020-10-24 18:52:52 +0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,9 @@
+let menuToggle = document.querySelector('.menu-toggle');
+let menu = document.querySelector('.sidebar');
+menuToggle.addEventListener('click', function (e) {
+    e.preventDefault();
+    menu.classList.toggle('visible');
+
+})
+
+// console.log('__');
